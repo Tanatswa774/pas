0a2d73171cac07b210e1d19afb4f49f9e3d69028
Tanatswa774 <83225521+Tanatswa774@users.noreply.github.com>
2025-07-19 15:13:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE = "https://b14527d1e5d9.ngrok-free.app";
 
 
 function LogsViewer() {
